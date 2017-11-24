9e185ca435ab0174caa3dab3b4f82e72e29b2a53
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2017-11-24 14:22:58 +0100

    Added simple array

--- Maventest/src/spillogik/Spil.java
+++ Maventest/src/spillogik/Spil.java
@@ -1,5 +1,11 @@
 package spillogik;
 
 public class Spil {
+	final static int MIN_POINTS = 0;
+
+	int FieldNumb = 24;
+	int 	Attribute = 4;
+	int Feilds[][] = new int [FieldNumb][Attribute];  
+
 
 }
