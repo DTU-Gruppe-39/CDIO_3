803260a2f7edad6b0b4353f26750edf2e19183ef
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2017-12-01 13:20:28 +0100

    Change 'spil' to 'Name'

--- Maventest/src/Main.java
+++ Maventest/src/Main.java
@@ -2,14 +2,14 @@ import java.io.IOException;
 
 import boundary.GUI_Test;
 import entity.ListOfPlayers;
-import gamelogic.Spil;
+import gamelogic.Game;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		GUI_Test.GUILauncher();
-		Spil.fillFields();
+		Game.fillFields();
 		ListOfPlayers list = new ListOfPlayers((GUI_Test.getNumberOfPlayers() + 1)); //Players index in array needs to start at 1, because 0 = to no owner on a field
 		list.addplayer(GUI_Test.getNumberOfPlayers());		
-		Spil.gameLogic();
+		Game.gameLogic();
 	}
 }
