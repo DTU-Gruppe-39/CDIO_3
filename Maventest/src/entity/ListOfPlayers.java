2647c6dd4f330d5f16b01dabd646346a9ba6e899
Patrick Marcell Madsen <32563752+DtuPat@users.noreply.github.com>
2017-12-01 10:52:59 +0100

    commented out unnecesary code

--- Maventest/src/entity/ListOfPlayers.java
+++ Maventest/src/entity/ListOfPlayers.java
@@ -36,11 +36,11 @@ public class ListOfPlayers {
 //		}		
 //		return false;
 //	}
-
-	public int nextPlayer(int index){
-		index = ++index % Players.length;
-		return index;
-	}
+//
+//	public int nextPlayer(int index){
+//		index = ++index % Players.length;
+//		return index;
+//	}
 	public static void addFunds(int amountOfPlayers) {
 		switch (amountOfPlayers) {
 		case 2:
